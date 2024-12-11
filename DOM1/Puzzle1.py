ac636da5bdc3caadf0d114e1ae8bd621e3d7e5ce
will.cole <will.david.cole@gmail.com>
2024-12-11 12:44:38 +0000

    Advent of code puzzle 1

---
+++ DOM1/Puzzle1.py
@@ -0,0 +1,23 @@
+from os.path import dirname
+
+def sumOfDifferences(filePath:str) -> int:
+    LocationIdList1 = []
+    LocationIdList2 = []
+    
+    with open(filePath,'r') as data:
+        for row in data:
+            row = row.replace('\n','')
+            x, y = row.split(',')
+            LocationIdList1.append(int(x))
+            LocationIdList2.append(int(y.replace('\n','')))
+    
+    LocationIdList1.sort(); LocationIdList2.sort();
+    return sum([abs(x - y) for x, y in zip(LocationIdList1,LocationIdList2)])
+
+def main():
+    currentPath = dirname(__file__)
+    dataListFile = currentPath + '\\Puzzle1Datasets.csv'
+    return sumOfDifferences(dataListFile)
+            
+if __name__ == "__main__":
+    main()
